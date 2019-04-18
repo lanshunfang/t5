431808eb50b634199e4c2ac601cd8c5de3ee849d
Beijing Shouwang Church <shwchurch3@gmail.com>
2019-04-18 15:35:50 +0000

    update sync

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,93 +1,179 @@
 #!/bin/bash
 
+protectedMp3FromDeletedRequiredInMarkdownFileNamePattern=2019
+
+tmpPathPrefix=/home/ec2-user/hugo/tmp/
+hugoExportedPath=${tmpPathPrefix}/wp-hugo-delta-processing
+
 log=/home/ec2-user/hugo/github/sync.log
+
+githubHugoPath=/home/ec2-user/hugo/github/t5/
+wodrePressHugoExportPath=/home/ec2-user/data/shwchurch/web/wp-content/plugins/wordpress-to-hugo-exporter
+
+
 echo "" > ${log}
 echo  "/home/ec2-user/hugo/github/hugo-theme/sync.sh > /home/ec2-user/hugo/github/sync.log 2>&1 &"
 
-echo "[INFO] Generating Markdown files from Wordpress "
-cd /home/ec2-user/data/shwchurch/web/wp-content/plugins/wordpress-to-hugo-exporter-master
-rm -rf /home/ec2-user/hugo/tmp/*
-php hugo-export-cli.php ~/hugo/tmp/ 
 
-cd /home/ec2-user/hugo/tmp/wp-hugo-tmp
+echo "[INFO] Cleanup ${hugoExportedPath}"
+if [[ ! -z "$hugoExportedPath" && -d "${hugoExportedPath}" ]]; then
+	rm -rf ${hugoExportedPath}
+fi
+
+echo "[INFO] Generating Markdown files from Wordpress "
+cd ${wodrePressHugoExportPath}
+#git stash
+#git pull
+
+# php hugo-export-cli.php ${tmpPathPrefix} > /home/ec2-user/hugo/github/sync.log 2>&1 &
+php hugo-export-cli.php ${tmpPathPrefix} 
+
+#cd /home/ec2-user/hugo/tmp/wp-hugo-tmp
+cd ${hugoExportedPath}
 
 #while [[ 1 ]]; do du -sh .; sleep 5; done;
 
+echo "[INFO] Remove file more than ${fileSizeOfFilesToRemove} that is not required from ${protectedMp3FromDeletedRequiredInMarkdownFileNamePattern}"
+postDir=${hugoExportedPath}/posts
+uploadsDir=${hugoExportedPath}/wp-content/uploads/
+cd ${postDir}
+allMp3RequiredDescriptor=uploaded-files-required.txt 
+allMp3Descriptor=uploaded-files.txt
+allMp3ToDeleteDescriptor=uploaded-files-to-delete.txt 
 
-echo "[INFO] Remove file more than ${fileSizeOfFilesToRemove}"
+grep -iRl "\.mp3" ./ | grep ${protectedMp3FromDeletedRequiredInMarkdownFileNamePattern} | xargs cat | grep "/.*\.mp3>" | perl -pe "s|.*/(.*?\.mp3).*|\1|g"  > ${allMp3RequiredDescriptor}
+echo "" > ${uploadsDir}/${allMp3ToDeleteDescriptor}
+
 fileSizeOfFilesToRemove=+1M
-cd /home/ec2-user/hugo/tmp/wp-hugo-tmp/wp-content/uploads/
-find . -type f -size ${fileSizeOfFilesToRemove} -printf '%s %p\n' | sort -nr | awk '{print $2}' | grep -v 2019  | xargs -I {} rm "{}"
+
+cd ${uploadsDir}
+#find . -type f -size ${fileSizeOfFilesToRemove} -printf '%s %p\n' | sort -nr | awk '{print $2}' | grep -v 2019  | xargs -I {} rm "{}"
+# find wp-content/uploads/ -type f -size ${fileSizeOfFilesToRemove} -printf '%s %p\n' | sort -nr | awk '{print $2}'  > ${allMp3Descriptor}
+find . -type f -size ${fileSizeOfFilesToRemove} -printf '%s %p\n' | sort -nr | awk '{print $2}'  > ${allMp3Descriptor}
+
+echo "[INFO] Generating all files to delete"
+while IFS='' read -r line || [[ -n "$line" ]]; do
+
+	isMp3Required=$(cat ${postDir}/${allMp3RequiredDescriptor} | xargs -I {}  bash -c "[[ \"${line}\" =~ \"{}\" ]] && echo {}" )
+
+	if [[ -z "$isMp3Required" ]];then
+		echo $line >> ${uploadsDir}/${allMp3ToDeleteDescriptor} 
+	else
+		echo "[INFO] Skip marking deletion: '$line' as it is required"
+	fi
+
+	  
+done < "${uploadsDir}/${allMp3Descriptor}"
+
+echo "[INFO] Delete files in ${uploadsDir}/${allMp3ToDeleteDescriptor}"
+
+cd ${uploadsDir}
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+
+	if [[ ! -z "$line" ]];then
+		rm $line
+	fi
+
+	  
+done < "${uploadsDir}/${allMp3ToDeleteDescriptor}"
 
 
-echo "[INFO] Copy all contents into Hugo folder"
+echo "[INFO] Delete other unnecessary files"
 
-rm -rf /home/ec2-user/hugo/github/t5/content/*
-cp -r /home/ec2-user/hugo/tmp/wp-hugo-tmp/* /home/ec2-user/hugo/github/t5/content/
+rm -rf ./ftp/choir-mp3/
 
+echo "[INFO] Copy all contents into Hugo folder for publishing"
+
+rm -rf ${githubHugoPath}/content/*
+if [[ ! -z "${hugoExportedPath}" && -d "${hugoExportedPath}"  ]];then
+	#cp -nr ${hugoExportedPath}/* ${githubHugoPath}/content/
+	cp -r ${hugoExportedPath}/* ${githubHugoPath}/content/
+fi
 
 
 ## file: ./strip-special-char.sh >/dev/null 2>&1 &
 
 
-echo "[INFO] Stripping all links including special chars"
+#echo "[INFO] Stripping all links including special chars"
 
-cd /home/ec2-user/hugo/github/t5/content/posts
+#cd /home/ec2-user/hugo/github/t5/content/posts
 
-echo "" > special-chars.txt 
-echo "" > special-chars-shorted.txt 
+##echo "" > special-chars.txt 
+##echo "" > special-chars-shorted.txt 
+##
+##grep -iRl "^url: /20" ./ | xargs cat | grep "^url: " | sed 's/url: //' >> special-chars.txt 2>/dev/null
 
-grep -iRl "^url: /20" ./ | xargs cat | grep "^url: " | sed 's/url: //' >> special-chars.txt 2>/dev/null
+##declare -a SpecialChars=(
+##	"　" 
+##	"，" 
+##	"-" 
+##	"—" 
+##	"——" 
+##	"－－" 
+##	" " 
+##	"”" 
+##	"“" 
+##	"”" 
+##	"？" 
+##	"：" 
+##	"！" 
+##	"_" 
+##	"（" 
+##	"）" 
+##	"《" 
+##	"》" 
+##	"•" 
+##	"、" 
+##	"：" 
+##	"、" 
+##	"："
+##	"＠"
+##)
+##
+##while IFS='' read -r line || [[ -n "$line" ]]; do
+##
+##	escapedLine=${line}
+##
+##	for SpecialChar in "${SpecialChars[@]}"; do
+##		escapedLine=$(printf '%s\n' "${escapedLine//${SpecialChar}/}")
+##	done
+##
+##	if [[ ! -z "${escapedLine}" && "${escapedLine}" != "${line}" ]]; then
+##
+##		pattern="s#${line}#${escapedLine}#g"
+##		echo "${pattern}" >> ./special-chars-shorted.txt
+##		echo "[INFO] ${pattern}"
+##
+##		find . -type f -name "*.md" -exec sed -i "${pattern}" {} \; >/dev/null 2>&1
+##	fi
+##	
+##    
+##done < "./special-chars.txt"
+##
 
-declare -a SpecialChars=(
-	"，" 
-	"-" 
-	"—" 
-	"——" 
-	"－－" 
-	" " 
-	"”" 
-	"“" 
-	"”" 
-	"？" 
-	"：" 
-	"！" 
-	"_" 
-	"（" 
-	"）" 
-	"《" 
-	"》" 
-	"•" 
-	"、" 
-	"：" 
-	"、" 
-	"："
+echo "[INFO] Replace all special chars in Markdown Title"
+
+cd ${githubHugoPath}/content/posts
+
+declare -a SpecialCharsInTitle=(
+        '@::＠'
 )
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
-
-	escapedLine=${line}
-
-	for SpecialChar in "${SpecialChars[@]}"; do
-		escapedLine=$(printf '%s\n' "${escapedLine//${SpecialChar}/}")
-	done
-
-	if [[ ! -z "${escapedLine}" && "${escapedLine}" != "${line}" ]]; then
-
-		pattern="s#${line}#${escapedLine}#g"
-		echo "${pattern}" >> ./special-chars-shorted.txt
-		echo "[INFO] ${pattern}"
-
-		find . -type f -name "*.md" -exec sed -i "${pattern}" {} \; >/dev/null 2>&1
-	fi
-	
-    
-done < "./special-chars.txt"
+for SpecialChar in "${SpecialCharsInTitle[@]}"; do
+        KEY="${SpecialChar%%::*}"
+        VALUE="${SpecialChar##*::}"
+        pattern="s#${KEY}#${VALUE}#g"
+        find . -type f -name "*.md" -exec sed -i "${pattern}" {} \;
+done
 
 
-cd /home/ec2-user/hugo/github/t5/
+cd ${githubHugoPath}/bin/
+
+echo "[INFO] Deploy and publish to github pages"
+./deploy.sh
 # cd /home/ec2-user/hugo/github/hugo-theme/
-hugo server -D --baseUrl=http://54.151.194.5:1313/ --bind=0.0.0.0
+#hugo server -D --baseUrl=http://54.151.194.5:1313/ --bind=0.0.0.0
 
 
 
